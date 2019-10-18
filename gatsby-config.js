29abfb242c66856bd06e36e76951650e0f0bb0e7
daniellesantana <daniellesantanaf@gmail.com>
2019-10-18 17:24:19 -0300

    delete and change files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
